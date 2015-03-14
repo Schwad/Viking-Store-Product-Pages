e1c2ef47dbc950e8325ec63241f9748b00e93829
Daniel Au <dakmau@gmail.com>
2015-03-14 19:36:38 -0400

    Working products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,28 @@
 class ProductsController < ApplicationController
   layout "user", only: [:index, :new, :show, :edit]
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def list_of_category_names
+    Category.all.each_with_object([]) do |cat, list|
+      list << cat.name
+    end
+  end
+
+  def whitelisted_product_params
+    clean_up_money
+    params.require(:product).permit(:name, :price, :category_id)
+  end
+
+  def clean_up_money
+    params[:product][:price] = params[:product][:price][1..-1] if params[:product][:price][0] == "$"
+  end
 end
