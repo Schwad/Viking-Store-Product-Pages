7b87324412b4c535b62d2fa7563e852f7ddd3267
Schwad <nicholas.schwaderer@gmail.com>
2015-03-15 19:14:26 +0000

    start work on shopping cart

---
+++ app/controllers/shopping_carts_controller.rb
@@ -0,0 +1,24 @@
+class ShoppingCartsController < ApplicationController
+
+  def edit
+    
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
